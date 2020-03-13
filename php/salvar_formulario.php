f2f105bb98ae835a7ba7c893da21e59b31d8bd3d
Vitor Peres <42453409+vitorperesss@users.noreply.github.com>
2020-03-13 11:16:17 -0300

    Update salvar_formulario.php

--- php/salvar_formulario.php
+++ php/salvar_formulario.php
@@ -1,6 +1,6 @@
 <?php
 
-$conect = mysqli_connect("localhost", "root", "","test");
+$conect = mysqli_connect("localhost", "root", "","biblioteca");
 
 // Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
 if (!$conect) die ("<h1>Falha na conexão com o Banco de Dados!</h1>");
